22097a806c18786a155083d6f9ddcac38e0bb8a0
John Craigie <trojansd@gmail.com>
2016-01-21 23:04:02 +0000

    Add 4.3.2

--- week-4/math/my_solution.rb
+++ week-4/math/my_solution.rb
@@ -1,22 +1,22 @@
 # Math Methods
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # Your Solution Below
 
 def add(num_1, num_2)
- #your code here
+  p num_1 + num_2
 end
 
 def subtract(num_1, num_2)
- #your code here
+  p num_1 - num_2
 end
 
 def multiply(num_1, num_2)
- #your code here
+  p num_1 * num_2
 end
 
 def divide(num_1, num_2)
- #your code here
+  p num_1.to_i / num_2.to_i
 end
