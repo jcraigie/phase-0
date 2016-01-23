6b72421bfcf7c56b897c63d829559a637366e748
John Craigie <trojansd@gmail.com>
2016-01-23 18:07:35 +0000

    Add 4.4

--- week-4/errors.rb
+++ week-4/errors.rb
@@ -88,18 +88,18 @@ def cartman_says(offensive_message)
   puts offensive_message
 end
 
-cartman_says
+cartman_says("But moooooommm")
 
 # 1. What is the line number where the error occurs?
-#
+#87
 # 2. What is the type of error message?
-#
+#wrong number of arguments
 # 3. What additional information does the interpreter provide about this type of error?
-#
+#(0 for 1) (ArgumentError)
 # 4. Where is the error in the code?
 #
 # 5. Why did the interpreter give you this error?
-#
+#It was expecting an argument but received none.
 
 
 
@@ -109,65 +109,73 @@ def cartmans_lie(lie, name)
   puts "#{lie}, #{name}!"
 end
 
-cartmans_lie('A meteor the size of the earth is about to hit Wyoming!')
+cartmans_lie('A meteor the size of the earth is about to hit Wyoming!', 'Kyle')
 
 # 1. What is the line number where the error occurs?
-#
+#108
 # 2. What is the type of error message?
-#
+# wrong number of arguments
 # 3. What additional information does the interpreter provide about this type of error?
-#
+#(1 for 2) (ArgumentError)
 # 4. Where is the error in the code?
-#
+#112 in main
 # 5. Why did the interpreter give you this error?
-#
+#It was expecting 2 arguments but recieved only one.
 
 # --- error -------------------------------------------------------
 
-5 * "Respect my authoritay!"
+#5 * "Respect my authoritay!"
 
 # 1. What is the line number where the error occurs?
-#
+#127
 # 2. What is the type of error message?
-#
+#`*': String can't be coerced into Fixnum
 # 3. What additional information does the interpreter provide about this type of error?
-#
+#(TypeError)
 # 4. Where is the error in the code?
-#
+#127 in main
 # 5. Why did the interpreter give you this error?
-#
+# A thing cannot be multiplied by "Respect my authoritay!" as it is not a number
 
 # --- error -------------------------------------------------------
 
-amount_of_kfc_left = 20/0
+amount_of_kfc_left = 20#/0
 
 
 # 1. What is the line number where the error occurs?
-#
+#142
 # 2. What is the type of error message?
-#
+#`/': divided by 0
 # 3. What additional information does the interpreter provide about this type of error?
-#
+#(ZeroDivisionError)
 # 4. Where is the error in the code?
-#
+#142 in main
 # 5. Why did the interpreter give you this error?
-#
+#Cannot divide by zero
 
 # --- error -------------------------------------------------------
 
-require_relative "cartmans_essay.md"
+#require_relative "cartmans_essay.md"
 
 # 1. What is the line number where the error occurs?
-#
+#158
 # 2. What is the type of error message?
-#
+#`require_relative': cannot load such file
 # 3. What additional information does the interpreter provide about this type of error?
-#
+#(LoadError)
 # 4. Where is the error in the code?
-#
+#158 in main
 # 5. Why did the interpreter give you this error?
-#
+#the file "cartmans_essay.md" is not available
 
 
 # --- REFLECTION -------------------------------------------------------
 # Write your reflection below as a comment.
+#Which error was the most difficult to read?
+# hardest error message to read was probably #8 (multiplying by a string) because it was worded in the least straightforward way of all of the samples.
+#How did you figure out what the issue with the error was?
+#I looked at the code line the error message pointed me too and saw the problem right away.
+#Were you able to determine why each error message happened based on the code? 
+#yes
+#When you encounter errors in your future code, what process will you follow to help you debug?
+#Check the error message for the location of the error and tpye and then go to that location and work it out.
