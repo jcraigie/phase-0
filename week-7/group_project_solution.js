4fb389b85b83cb47e08000c1476e8f20559fb18f
John Craigie <trojansd@gmail.com>
2016-02-15 06:06:14 +0000

    Add group telephone

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -6,6 +6,16 @@
 //assert test
         // Create an assert function that accepts an array of integers as an agrument. If the input is an integer return true, if its not throw an error message.
 
+function assert(array){
+    for (var x = 0; x < array.length; x+=1){
+        if (array[x] % 1 === 0){
+            true;
+        }else{
+            throw "there is a non-integer present";
+        }   
+    }
+}
+console.log(assert([1,2,3,4,5]))
 
 // tests for sum
         // Create a function name sum that accepts an array of integers as an argument
@@ -15,7 +25,16 @@
         // Call the sum function with even numbers
         // Call the sum function with odd numbers
 
+function sum(array){
+    var total = 0;
+    for (var x = 0; x < array.length; x += 1 ){
+            total += array[x];
+        }
+        return total;
+}
 
+console.log(sum([2,4,6,8,10]))
+console.log(sum([1,3,5,7,9,11]))
 
 // tests for mean
         // Create a function named mean that accepts an array of integers as an argument
@@ -25,6 +44,16 @@
         // Call the mean function with even numbers
         // Call the mean function with odd numbers
 
+function mean(array){
+    var total = 0;
+    for (var x = 0; x < array.length; x += 1 ){
+            total += array[x];
+        }
+        return total / (array.length);
+}
+
+console.log(mean([2,4,6,8,10]))
+console.log(mean([1,3,5,7,9,11]))
 
 // tests for median
         // Create a function median that accepts an array of integers as an argument
@@ -38,3 +67,19 @@
         //Return the median value
         // Call the median function with even numbers
         // Call the median function with odd numbers
+        
+function median(array){
+    var sorted = array.sort(function(a, b){return a-b});
+    var length = array.length;
+    var median = Math.floor((array.length - 1) / 2);
+    if (length % 2 == 1){
+        return array[median];
+    }else{
+        return array[median];
+    }
+
+}
+
+
+console.log(median([2,4,6,8,12,14,16,18]))
+console.log(median([1,3,5,7,9,11,13,15,17,19,21,23,25]))
