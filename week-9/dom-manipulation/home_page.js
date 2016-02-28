9f280028412bd680aef4e59191c5e115ab2b3d54
John Craigie <trojansd@gmail.com>
2016-02-28 00:13:35 +0000

    Add exercise

---
+++ week-9/dom-manipulation/home_page.js
@@ -0,0 +1,40 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge with: Austin Dorff.
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+// Set up
+
+
+
+// Release 1:
+var div_r_1 = document.getElementById("release-0");
+div_r_1.className = "done";
+
+// Release 2:
+var div_r_2 = document.getElementById('release-1');
+div_r_2.style.display = "none";
+
+// Release 3:
+var div_r_3 = document.getElementsByTagName('h1')[0];
+div_r_3.innerHTML = "I completed release 2.";
+
+
+// Release 4:
+var div_r_4 = document.getElementById('release-3');
+div_r_4.style.backgroundColor = "#955251";
+
+// Release 5:
+var div_r_5 = document.getElementsByClassName("release-4");
+   for (var i = 0; i < div_r_5.length; i++) {
+    div_r_5[i].style.fontSize = "2em";
+  }
+
+// Release 6:
+var template = document.getElementById('hidden');
+document.body.appendChild(template.content.cloneNode(true));
+
---
+++ week-9/ruby-review-2/ruby-review.rb
@@ -0,0 +1,147 @@
+# Numbers to English Words
+
+
+# I worked on this challenge with: Austin Dorff.
+# This challenge took me [.5] hours.
+
+
+# Pseudocode
+
+
+
+# Initial Solution
+# def in_words(number)
+#   base_words = {
+#     0 => "Zero",
+#     1 => "One",
+#     2 => "Two",
+#     3 => "Three",
+#     4 => "Four",
+#     5 => "Five",
+#     6 => "Six",
+#     7 => "Seven",
+#     8 => "Eight",
+#     9 => "Nine",
+#     10 => "Ten",
+#     11 => "eleven",
+#     12 => "Twelve",
+#     13 => "Thirteen",
+#     14 => "Fourteen",
+#     15 => "Fifteen",
+#     16 => "Sixteen",
+#     17 => "Seventeen",
+#     18 => "Eighteen",
+#     19 => "Nineteen",
+#     20 => "Twenty",
+#     30 => "Thirty",
+#     40 => "Fourty",
+#     50 => "Fifty",
+#     60 => "Sixty",
+#     70 => "Seventy",
+#     80 => "Eighty",
+#     90 => "Ninety",
+#     100 => "One Hundred",
+#     1000 => "O"
+#   }
+  
+#   if (base_words.has_key?(number))
+#     p base_words[number]
+#   else
+#       p base_words[get_highest_num(base_words, number)] + " " + base_words[number % get_highest_num(base_words, number)]
+#   end
+# end
+  
+# def get_places(num)
+#   counter = 0
+#   while (num > 1) 
+#     num /= 10;
+#     counter += 1
+#   end
+#   return counter
+# end
+
+# def get_highest_num(hash, number)
+#   if (number < 20)
+#     return number
+#   else
+#     return (number - (number % 10))
+#   end
+# end
+# for i in 0..100 do
+#   in_words(i)
+# end
+
+
+# Refactored Solution
+def in_words(number)
+  base_words = {
+    0 => "Zero",
+    1 => "One",
+    2 => "Two",
+    3 => "Three",
+    4 => "Four",
+    5 => "Five",
+    6 => "Six",
+    7 => "Seven",
+    8 => "Eight",
+    9 => "Nine",
+    10 => "Ten",
+    11 => "eleven",
+    12 => "Twelve",
+    13 => "Thirteen",
+    14 => "Fourteen",
+    15 => "Fifteen",
+    16 => "Sixteen",
+    17 => "Seventeen",
+    18 => "Eighteen",
+    19 => "Nineteen",
+    20 => "Twenty",
+    30 => "Thirty",
+    40 => "Fourty",
+    50 => "Fifty",
+    60 => "Sixty",
+    70 => "Seventy",
+    80 => "Eighty",
+    90 => "Ninety",
+    100 => "Hundred",
+    1000 => "Thousand",
+    1000000 => "Million"
+  }
+  if (base_words.has_key?(number))
+    if (number == 100) 
+      p base_words[1] + " " + base_words[number]
+    else
+      p base_words[number]
+    end
+  else
+      p base_words[get_highest_num(base_words, number)] + " " + base_words[number % get_highest_num(base_words, number)]
+  end
+end
+  
+def get_places(num)
+  counter = 0
+  while (num > 1) 
+    num /= 10;
+    counter += 1
+  end
+  return counter
+end
+
+def get_highest_num(hash, number)
+  if (number < 20)
+    return number
+  else
+    return (number - (number % 10))
+  end
+end
+for i in 0..100 do
+  in_words(i)
+end
+
+
+
+
+
+# Reflection
+
+
